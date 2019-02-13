a9d6624ca2108826c60453fcc69083f7745685b4
Steve Thompson <writetome51@gmail.com>
2019-02-12 22:47:19 -0700

    Added more tests. Added examples to README

--- README.md
+++ README.md
@@ -6,16 +6,74 @@ of every property listed in <b>properties</b>.
 The order you list the <b>properties</b> matters.  The first property performs the initial sort, and all
 objects are separated into groups by that property.  All subsequent properties separate the groups
 into smaller, more specific groups.  
-Each property in <b>properties</b> can contain dot-notation.
-
-<b>property</b> is a string that can include dot notation ( i.e,  `'property.subproperty.subsubproperty'` ) .  
-Note:  <b>property</b> does not have to be an object key. It can also be an array index.  
-If you are getting the value of a nested array index, here you need to use dot-notation and not  
-square braces.  Example: `'1.0' instead of [1][0]`
+Each property in <b>properties</b> can contain dot-notation ( i.e,  `'property.subproperty.subsubproperty'` ).
 
 ## Examples
 ```
+let persons = [
+	{name: {first: 'Danny', last: 'Jones'}, address:'800 N. First St.'},
+	{name: {first: 'Megan', last: 'Ferguson'}, address:'200 W. Elm St.'},
+	{name: {first: 'Michael', last: 'Watts'}, address:'100 S. Palm Way'},
+	{name: {first: 'William', last: 'Peltz'}, address:'2000 Lawrence Ave.'},
+	{name: {first: 'Robert', last: 'Walters'}, address:'200 W. Elm St.'},
+	{name: {first: 'Sara', last: 'Watts'}, address:'100 S. Palm Way'},
+	{name: {first: 'Carol', last: 'Jones'}, address:'800 N. First St.'}
+];
 
+groups = getGroupedByProperties(['address', 'name.last'], persons);
+
+/******************
+ groups is:
+ [
+    [
+       {name: {first: 'Michael', last: 'Watts'}, address: '100 S. Palm Way'},
+       {name: {first: 'Sara', last: 'Watts'}, address: '100 S. Palm Way'}
+    ],
+    [ {name: {first: 'Megan', last: 'Ferguson'}, address: '200 W. Elm St.'} ],
+    [ {name: {first: 'Robert', last: 'Walters'}, address: '200 W. Elm St.'} ],
+    [ {name: {first: 'William', last: 'Peltz'}, address: '2000 Lawrence Ave.'} ],
+    [
+       {name: {first: 'Danny', last: 'Jones'}, address: '800 N. First St.'},
+       {name: {first: 'Carol', last: 'Jones'}, address: '800 N. First St.'}
+    ]
+ ]
+ ******************/
+ 
+ 
+persons = [
+	{name:{first: 'Danny', last: 'Jones'}, email: 'd_jonesy500@yahoo.com', role: 'admin'},
+	{name:{first: 'Megan', last: 'Ferguson'}, email: 'fergie100@yahoo.com', role: 'user'},
+	{name:{first: 'Megan', last: 'Ferguson'}, email: 'fergie100@yahoo.com', role: 'super-admin'},
+	{name:{first: 'Danny', last: 'Jones'}, email: 'd_jonesy500@yahoo.com', role: 'user'},
+	{name:{first: 'Michael', last: 'Watts'}, email: 'watts_my_name@gmail.com', role: 'admin'},
+	{name:{first: 'Bill', last: 'Butler'}, email: 'b_butler999@hotmail.com', role: 'admin'},
+	{name:{first: 'Michael', last: 'Watts'}, email: 'watts_my_name@gmail.com', role: 'user'},
+	{name:{first: 'Danny', last: 'Jones'}, email: 'd_jonesy500@yahoo.com', role: 'super-admin'},
+	{name:{first: 'Bill', last: 'Butler'}, email: 'bbutler666@hotmail.com', role: 'user'},
+];
+
+groups = getGroupedByProperties(['name.last', 'email'], persons);
+
+/***************
+groups is:
+ [
+    [ { name: {first: 'Bill', last: 'Butler'}, email: 'b_butler999@hotmail.com', role: 'admin' } ],
+    [ { name: {first: 'Bill', last: 'Butler'}, email: 'bbutler666@hotmail.com', role: 'user' } ],
+    [
+       { name: {first: 'Megan', last: 'Ferguson'}, email: 'fergie100@yahoo.com', role: 'user' },
+       { name: {first: 'Megan', last: 'Ferguson'}, email: 'fergie100@yahoo.com', role: 'super-admin' }
+    ],
+    [
+       { name: {first: 'Danny', last: 'Jones'}, email: 'd_jonesy500@yahoo.com', role: 'admin' },
+       { name: {first: 'Danny', last: 'Jones'}, email: 'd_jonesy500@yahoo.com', role: 'user' },
+       { name: {first: 'Danny', last: 'Jones'}, email: 'd_jonesy500@yahoo.com', role: 'super-admin' }
+    ],
+    [
+       { name: {first: 'Michael', last: 'Watts'}, email: 'watts_my_name@gmail.com', role: 'admin' },
+       { name: {first: 'Michael', last: 'Watts'}, email: 'watts_my_name@gmail.com', role: 'user' }
+    ]
+ ]
+ ***********************/
 ```
 
 ## Installation
--- tests.js
+++ tests.js
@@ -69,4 +69,69 @@ console.log(groups);
     [ { height: 72, hair: 'blonde', eyes: 'green' } ],
     [ { height: 83, hair: 'red', eyes: 'hazel' } ]
  ]
- ******************/ 
+ ******************/
+persons = [
+    { name: { first: 'Danny', last: 'Jones' }, email: 'd_jonesy500@yahoo.com', address: '800 N. First St.' },
+    { name: { first: 'Megan', last: 'Ferguson' }, email: 'fergie100@yahoo.com', address: '200 W. Elm St.' },
+    { name: { first: 'Michael', last: 'Watts' }, email: 'watts_my_name@gmail.com', address: '100 S. Palm Way' },
+    { name: { first: 'William', last: 'Peltz' }, email: 'jonesy500@yahoo.com', address: '2000 Lawrence Ave.' },
+    { name: { first: 'Bill', last: 'Butler' }, email: 'bbutler999@hotmail.com', address: '2000 Lawrence Ave.' },
+    { name: { first: 'Robert', last: 'Walters' }, email: 'r_walters500@gmail.com', address: '200 W. Elm St.' },
+    { name: { first: 'Sara', last: 'Watts' }, email: 'sarawatts100@gmail.com', address: '100 S. Palm Way' },
+    { name: { first: 'Carol', last: 'Jones' }, email: 'c_jonesy500@yahoo.com', address: '800 N. First St.' },
+    { name: { first: 'Bill', last: 'Butler' }, email: 'bbutler666@hotmail.com', address: '6000 Raw Way' },
+];
+console.log(''); // just to add a line break.
+groups = index_1.getGroupedByProperties(['address', 'name.last'], persons);
+console.log(groups);
+/******************
+ Result:
+ [
+    [
+       {name: {first: 'Michael', last: 'Watts'}, email: 'watts_my_name@gmail.com', address: '100 S. Palm Way'},
+       {name: {first: 'Sara', last: 'Watts'}, email: 'sarawatts100@gmail.com', address: '100 S. Palm Way'}
+    ],
+    [ {name: {first: 'Megan', last: 'Ferguson'}, email: 'fergie100@yahoo.com', address: '200 W. Elm St.'} ],
+    [ {name: {first: 'Robert', last: 'Walters'}, email: 'r_walters500@gmail.com', address: '200 W. Elm St.'} ],
+    [ {name: {first: 'Bill', last: 'Butler'}, email: 'bbutler999@hotmail.com', address: '2000 Lawrence Ave.'} ],
+    [ {name: {first: 'William', last: 'Peltz'}, email: 'jonesy500@yahoo.com', address: '2000 Lawrence Ave.'} ],
+    [ {name: {first: 'Bill', last: 'Butler'}, email: 'bbutler666@hotmail.com', address: '6000 Raw Way'} ],
+    [
+       {name: {first: 'Danny', last: 'Jones'}, email: 'd_jonesy500@yahoo.com', address: '800 N. First St.'},
+       {name: {first: 'Carol', last: 'Jones'}, email: 'c_jonesy500@yahoo.com', address: '800 N. First St.'}
+    ]
+ ]
+ ******************/
+persons = [
+    { name: { first: 'Danny', last: 'Jones' }, email: 'd_jonesy500@yahoo.com', role: 'admin' },
+    { name: { first: 'Megan', last: 'Ferguson' }, email: 'fergie100@yahoo.com', role: 'user' },
+    { name: { first: 'Megan', last: 'Ferguson' }, email: 'fergie100@yahoo.com', role: 'super-admin' },
+    { name: { first: 'Danny', last: 'Jones' }, email: 'd_jonesy500@yahoo.com', role: 'user' },
+    { name: { first: 'Michael', last: 'Watts' }, email: 'watts_my_name@gmail.com', role: 'admin' },
+    { name: { first: 'Bill', last: 'Butler' }, email: 'b_butler999@hotmail.com', role: 'admin' },
+    { name: { first: 'Michael', last: 'Watts' }, email: 'watts_my_name@gmail.com', role: 'user' },
+    { name: { first: 'Danny', last: 'Jones' }, email: 'd_jonesy500@yahoo.com', role: 'super-admin' },
+    { name: { first: 'Bill', last: 'Butler' }, email: 'bbutler666@hotmail.com', role: 'user' },
+];
+console.log(''); // just to add a line break.
+groups = index_1.getGroupedByProperties(['name.last', 'email'], persons);
+console.log(groups);
+/***************
+ [
+    [ { name: {first: 'Bill', last: 'Butler'}, email: 'b_butler999@hotmail.com', role: 'admin' } ],
+    [ { name: {first: 'Bill', last: 'Butler'}, email: 'bbutler666@hotmail.com', role: 'user' } ],
+    [
+       { name: {first: 'Megan', last: 'Ferguson'}, email: 'fergie100@yahoo.com', role: 'user' },
+       { name: {first: 'Megan', last: 'Ferguson'}, email: 'fergie100@yahoo.com', role: 'super-admin' }
+    ],
+    [
+       { name: {first: 'Danny', last: 'Jones'}, email: 'd_jonesy500@yahoo.com', role: 'admin' },
+       { name: {first: 'Danny', last: 'Jones'}, email: 'd_jonesy500@yahoo.com', role: 'user' },
+       { name: {first: 'Danny', last: 'Jones'}, email: 'd_jonesy500@yahoo.com', role: 'super-admin' }
+    ],
+    [
+       { name: {first: 'Michael', last: 'Watts'}, email: 'watts_my_name@gmail.com', role: 'admin' },
+       { name: {first: 'Michael', last: 'Watts'}, email: 'watts_my_name@gmail.com', role: 'user' }
+    ]
+ ]
+ ***********************/ 
